4c65dd5ffbaf23f8ceb57df2f12adbe267a673c4
samtheerapong <theerapong.khan@gmail.com>
2023-10-24 21:31:49 +0700

    up

--- modules/salers/views/status/_form.php
+++ modules/salers/views/status/_form.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use kartik\widgets\ColorInput;
 
 /** @var yii\web\View $this */
 /** @var app\modules\salers\models\Status $model */
@@ -14,7 +15,9 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'color')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'color')->widget(ColorInput::classname(), [
+        'options' => ['placeholder' => 'Select color ...'],
+    ]); ?>
 
     <div class="form-group">
         <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
