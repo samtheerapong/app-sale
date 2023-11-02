531f57a523af635604454863c670cbb7997398ce
samtheerapong <theerapong.khan@gmail.com>
2023-11-02 14:02:29 +0700

    up

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=sale-orders', // Modules salers 
+    'dsn' => 'mysql:host=localhost;dbname=nfc-app', // Modules salers 
     // 'dsn' => 'mysql:host=localhost;dbname=sale-order2', // Modules shop
     'username' => 'sam',
     'password' => 'sam',
