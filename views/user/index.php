a84ba5a605142e6b59b4b071f59c04a527822c88
samtheerapong <theerapong.khan@gmail.com>
2023-11-07 16:59:09 +0700

    up

--- views/user/index.php
+++ views/user/index.php
@@ -1,118 +1,118 @@
 <?php
 
 use app\models\User;
+use yii\bootstrap5\LinkPager;
 use yii\helpers\Html;
 use yii\helpers\Url;
-use yii\grid\ActionColumn;
-use kartik\grid\GridView;
-use kartik\widgets\Select2;
+use yii\widgets\Pjax;
 
 /** @var yii\web\View $this */
-/** @var app\models\UserSearch $searchModel */
+/** @var app\modules\sauce\models\RawSauceSearch $searchModel */
 /** @var yii\data\ActiveDataProvider $dataProvider */
 
-$this->title = Yii::t('app', 'Users');
+$this->title = Yii::t('app', 'Request Repair');
 $this->params['breadcrumbs'][] = $this->title;
+
 ?>
-<div class="user-index">
-    <p>
-        <!-- <?= Html::a('<i class="fa fa-plus"></i> ' . Yii::t('app', 'Create Data'), ['../../frontend/web/site/signup'], ['class' => 'btn btn-danger']) ?> -->
-        <?= Html::a('<i class="fa fa-plus"></i> ' . Yii::t('app', 'Create Data'), ['create'], ['class' => 'btn btn-danger']) ?>
-    </p>
+<div class="raw-sauce-index">
+    <div class="row">
+        <p>
+            <?= Html::a('<i class="fas fa-plus"></i> ' . Yii::t('app', 'Create New User'), ['create'], ['class' => 'btn btn-primary']) ?>
+        </p>
+        <?php Pjax::begin(); ?>
 
-    <div class="card border-secondary">
-        <div class="card-header text-white bg-secondary">
-            <?= Html::encode($this->title) ?>
+        <div class="row">
+            <?php // echo $this->render('_search', ['model' => $searchModel]); n
+            ?>
         </div>
-        <div class="card-body">
-            <div class="table-responsive">
-                <?= GridView::widget([
-                    'dataProvider' => $dataProvider,
-                    'filterModel' => $searchModel,
-                    'columns' => [
-                        ['class' => 'yii\grid\SerialColumn'],
 
-                        // 'id',
-                        'username',
-                        'thai_name',
-                        // 'auth_key',
-                        // 'password_hash',
-                        //'password_reset_token',
-                        'email:email',
-                        // 'status',
-                        [
-                            'attribute' => 'status',
-                            'format' => 'raw',
-                            'value' => function ($model) {
-                                return $model->status === User::STATUS_ACTIVE
-                                    ? '<span class="badge" style="background-color: #1A5D1A">Active</span>'
-                                    : '<span class="badge" style="background-color: #FE0000">In Active</span>';
-                            },
-                            'filter' => Select2::widget([
-                                'model' => $searchModel,
-                                'attribute' => 'status',
-                                'data' => [
-                                    User::STATUS_ACTIVE     => Yii::t('app', 'Active'),
-                                    User::STATUS_INACTIVE   => Yii::t('app', 'In Active'),
-                                ],
-                                'options' => ['placeholder' => Yii::t('app', 'Select...')],
-                                'language' => 'th',
-                                'pluginOptions' => [
-                                    'allowClear' => true
-                                ],
-                            ]),
-                        ],
-                        // 'created_at:date',
-                        //'updated_at',
-                        //'verification_token',
-                        // 'role',
-                        [
-                            'attribute' => 'role',
-                            'format' => 'raw',
-                            'value' => function ($model) {
-                                $roleLabels = [
-                                    User::ROLE_ADMIN    => '<span class="badge" style="background-color: #0079FF">Admin</span>',
-                                    User::ROLE_QA       => '<span class="badge" style="background-color: #FFB84C">QA</span>',
-                                    User::ROLE_SALE     => '<span class="badge" style="background-color: #7149C6">Sale</span>',
-                                    User::ROLE_MANAGER  => '<span class="badge" style="background-color: #F31559">Manager</span>',
-                                    User::ROLE_USER     => '<span class="badge" style="background-color: #6C3428">User</span>',
-                                ];
+        <div class="row">
+            <?php
+            foreach ($dataProvider->getModels() as $model) : ?>
+                <div class="col-md-3 ol-sm-6">
+                    <div class="card mb-2">
+                        <div class="card-header" style="background-color: 
+                        <?= $model->status === User::STATUS_ACTIVE ? '#1A5D1A' : '#FE0000' ?>
+                        ; color: #fff;">
+                            <a href="<?= Url::toRoute(['view', 'id' => $model->id]); ?>" class="link-light">
+                                <h5>
+                                    <?= Html::encode($model->username) ?> :
 
-                                return isset($roleLabels[$model->role]) ? $roleLabels[$model->role] : '';
-                            },
-                            'filter' => Select2::widget([
-                                'model' => $searchModel,
-                                'attribute' => 'role',
-                                'data' => [
-                                    User::ROLE_ADMIN    => Yii::t('app', 'Admin'),
-                                    User::ROLE_QA       => Yii::t('app', 'QA'),
-                                    User::ROLE_SALE     => Yii::t('app', 'Sale'),
-                                    User::ROLE_MANAGER  => Yii::t('app', 'Manager'),
-                                    User::ROLE_USER     => Yii::t('app', 'User'),
-                                ],
-                                'options' => ['placeholder' => Yii::t('app', 'Select...')],
-                                'language' => 'th',
-                                'pluginOptions' => [
-                                    'allowClear' => true
-                                ],
-                            ]),
-                        ],
-                        [
-                            // 'class' => ActionColumn::class,
-                            'class' => 'kartik\grid\ActionColumn',
-                            'header' => 'จัดการ',
-                            'headerOptions' => ['style' => 'width: 140px;'],
-                            'contentOptions' => ['class' => 'text-center'], // จัดตรงกลาง
-                            'buttonOptions' => ['class' => 'btn btn-sm btn-outline-primary btn-group'],
-                            'urlCreator' => function ($action, User $model, $key, $index, $column) {
-                                return Url::toRoute([$action, 'id' => $model->id]);
-                            }
-                        ],
-                    ],
-                ]); ?>
+                                    <small>
+                                        <?= Html::encode($model->thai_name ?? '') ?>
+                                    </small>
+                                </h5>
+                            </a>
+                        </div>
+
+                        <div class="card-body">
 
 
-            </div>
+                            <p class="card-text"><b><?= Yii::t('app', 'Username') ?></b> : <?= Html::encode($model->username) ?></p>
+                            <p class="card-text"><b><?= Yii::t('app', 'Thai Name') ?></b> : <?= Html::encode($model->thai_name) ?></p>
+                            <p class="card-text"><b><?= Yii::t('app', 'Email') ?></b> : <?= \yii\helpers\Html::a($model->email, 'mailto:' . $model->email) ?></p>
+                            <p class="card-text"><b><?= Yii::t('app', 'Location') ?></b> : <?= Html::encode($model->username) ?></p>
+
+                            <p class="card-text"><b><?= Yii::t('app', 'Status') ?></b> :
+                                <span class="badge" style="color: <?= $model->status === User::STATUS_ACTIVE ? '#1A5D1A' : '#FE0000' ?>; font-size: 16px;"><?= $model->status === User::STATUS_ACTIVE ? 'Active' : 'Not Active' ?></span>
+                            </p>
+
+                            <p class="card-text"><b><?= Yii::t('app', 'Role') ?></b> :
+                                <?php
+                                if ($model->role === User::ROLE_ADMIN) {
+                                    echo 'Admin';
+                                } elseif ($model->role === User::ROLE_MANAGER) {
+                                    echo 'Manager';
+                                } elseif ($model->role === User::ROLE_QA) {
+                                    echo 'QA';
+                                } elseif ($model->role === User::ROLE_SALE) {
+                                    echo 'Sale';
+                                } else {
+                                    echo 'User';
+                                }
+                                ?>
+                            </p>
+
+                            <p class="card-text"><b><?= Yii::t('app', 'Created At') ?></b> : <?= Html::encode(Yii::$app->formatter->asDate($model->created_at, 'dd MMMM YYYY')) ?></p>
+
+
+                        </div>
+
+                        <div class="card-footer text-center">
+                            <div class="btn-group btn-group-xs" role="group">
+                                <?= Html::a('<i class="fas fa-eye"></i>', ['view', 'id' => $model->id], ['class' => 'btn btn-outline-dark btn-sm', 'title' => Yii::t('app', 'View'), 'aria-label' => Yii::t('app', 'View')]) ?>
+                                <?= Html::a('<i class="fas fa-pencil"></i>', ['update', 'id' => $model->id], ['class' => 'btn btn-outline-dark btn-sm', 'title' => Yii::t('app', 'Update'), 'aria-label' => Yii::t('app', 'Update')]) ?>
+                                <?= Html::a('<i class="fas fa-trash-can"></i>', ['delete', 'id' => $model->id], [
+                                    'class' => 'btn btn-outline-dark btn-sm', 'title' => Yii::t('app', 'Delete'), 'aria-label' => Yii::t('app', 'Delete'),
+                                    'data' => [
+                                        'confirm' => 'Are you sure you want to delete this item?',
+                                        'method' => 'post',
+                                    ],
+                                ]) ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            <?php endforeach; ?>
+
         </div>
+        <div class="col-md-12">
+            <?php
+            echo LinkPager::widget([
+                'pagination' => $dataProvider->pagination,
+                'maxButtonCount' => 5,
+                'firstPageLabel' => Yii::t('app', 'First'),
+                'lastPageLabel' => Yii::t('app', 'Last'),
+                'options' => ['class' => 'pagination justify-content-center'],
+                'linkContainerOptions' => ['class' => 'page-item'],
+                'linkOptions' => ['class' => 'page-link'],
+            ]);
+            ?>
+
+        </div>
+
+        <?php Pjax::end(); ?>
+
     </div>
 </div>
